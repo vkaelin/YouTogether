5c6fbaec29e6afcdb37cb3f7619a0593d1e5b956
Kalaneee <33511688+Kalaneee@users.noreply.github.com>
2018-06-16 15:09:50 +0200

    Dont use ES6 syntax in js

--- app/assets/javascripts/navigation.js
+++ app/assets/javascripts/navigation.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
 
   window.addEventListener('scroll', function() {
-    let header = document.querySelector('.banner');
+    var header = document.querySelector('.banner');
     if (window.scrollY > 50) {
       header.firstElementChild.className = 'global-header-animated';
     } else {
