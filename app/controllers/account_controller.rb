269ccf5e54d203daf5e643181c64d8e1aa051d13
Kalaneee <33511688+Kalaneee@users.noreply.github.com>
2018-06-19 15:13:41 +0200

    Different headers if logged and account modifications page

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -1,4 +1,17 @@
 class AccountController < ApplicationController
+  before_action :ensure_authenticated
+  
   def edit
+  end
+
+  def update
+    current_user.update(user_params)
+    redirect_to(account_path)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :name, :avatar)
   end
 end
