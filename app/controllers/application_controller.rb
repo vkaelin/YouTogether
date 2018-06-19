01c3736ac9405232f296333b1c2483c822b0dad7
Kalaneee <33511688+Kalaneee@users.noreply.github.com>
2018-06-19 17:18:53 +0200

    Start i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,16 @@ class ApplicationController < ActionController::Base
 
   helper_method :logged_in?, :current_user
 
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
   def logged_in?
     session[:user_id].present?
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,20 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'home#index'
+  scope '/:locale' do
+    root to: 'home#index'
 
-  resources :users
+    resources :users
 
-  resources :sessions, only: [:new, :create, :destroy]
+    resources :sessions, only: [:new, :create, :destroy]
 
-  resources :rooms
-  
-  get 'signup', to: 'users#new'
-  get 'login', to: 'sessions#new'
+    resources :rooms
 
-  get 'account', to: 'account#edit'
-  patch 'account', to: 'account#update'
+    get 'signup', to: 'users#new'
+    get 'login', to: 'sessions#new'
 
+    get 'account', to: 'account#edit'
+    patch 'account', to: 'account#update'
+  end
 
 end
